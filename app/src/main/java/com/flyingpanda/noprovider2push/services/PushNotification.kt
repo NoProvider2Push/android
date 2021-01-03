59197e6a953e3358e32790820ffe3c1c0966b201
p1gp1g <simon.gougeon@protonmail.com>
2021-01-03 13:48:57 +0100

    ACTION_REGISTRATION_(FAILED|REFUSED)

--- app/src/main/java/com/flyingpanda/noprovider2push/services/PushNotification.kt
+++ app/src/main/java/com/flyingpanda/noprovider2push/services/PushNotification.kt
@@ -12,9 +12,9 @@ fun sendMessage(context: Context, application: String, message: String){
     val token = getToken(context,application)!!
     val broadcastIntent = Intent()
     broadcastIntent.`package` = application
-    broadcastIntent.action = MESSAGE
-    broadcastIntent.putExtra("token", token)
-    broadcastIntent.putExtra("message", message)
+    broadcastIntent.action = ACTION_MESSAGE
+    broadcastIntent.putExtra(EXTRA_TOKEN, token)
+    broadcastIntent.putExtra(EXTRA_MESSAGE, message)
     context.sendBroadcast(broadcastIntent)
 }
 
@@ -22,9 +22,9 @@ fun sendEndpoint(context: Context, application: String, endpoint: String) {
     val token = getToken(context,application)!!
     val broadcastIntent = Intent()
     broadcastIntent.`package` = application
-    broadcastIntent.action = NEW_ENDPOINT
-    broadcastIntent.putExtra("token", token)
-    broadcastIntent.putExtra("endpoint", endpoint)
+    broadcastIntent.action = ACTION_NEW_ENDPOINT
+    broadcastIntent.putExtra(EXTRA_TOKEN, token)
+    broadcastIntent.putExtra(EXTRA_ENDPOINT, endpoint)
     context.sendBroadcast(broadcastIntent)
 }
 
@@ -32,8 +32,26 @@ fun sendUnregistered(context: Context, application: String, _token: String?){
     val token = _token?: getToken(context,application)!!
     val broadcastIntent = Intent()
     broadcastIntent.`package` = application
-    broadcastIntent.action = UNREGISTERED
-    broadcastIntent.putExtra("token", token)
+    broadcastIntent.action = ACTION_UNREGISTERED
+    broadcastIntent.putExtra(EXTRA_TOKEN, token)
+    context.sendBroadcast(broadcastIntent)
+}
+
+fun sendRegistrationFailed(context: Context, application: String, token: String, message: String){
+    val broadcastIntent = Intent()
+    broadcastIntent.`package` = application
+    broadcastIntent.action = ACTION_REGISTRATION_FAILED
+    broadcastIntent.putExtra(EXTRA_TOKEN, token)
+    broadcastIntent.putExtra(EXTRA_MESSAGE, message)
+    context.sendBroadcast(broadcastIntent)
+}
+
+fun sendRegistrationRefused(context: Context, application: String, token: String, message: String){
+    val broadcastIntent = Intent()
+    broadcastIntent.`package` = application
+    broadcastIntent.action = ACTION_REGISTRATION_REFUSED
+    broadcastIntent.putExtra(EXTRA_TOKEN, token)
+    broadcastIntent.putExtra(EXTRA_MESSAGE, message)
     context.sendBroadcast(broadcastIntent)
 }
 
